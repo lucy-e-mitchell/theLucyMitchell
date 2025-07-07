027fb48f8d373c94e863b463cdfc06e17901abd5
lucy-e-mitchell <lucymitchell574@gmail.com>
2025-07-07 09:50:50 +0100

    custom domain setup

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/theLucyMitchell/' // update to repo name
+  base: '/' // update to repo name
 })
