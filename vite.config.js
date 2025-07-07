1711ab3cb782bbcb1d26c91e323d3e906b9c0b8c
lucy-e-mitchell <lucymitchell574@gmail.com>
2025-07-07 03:42:35 +0100

    update repo name

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ds-cv/' // update to repo name
+  base: '/theLucyMitchell/' // update to repo name
 })
